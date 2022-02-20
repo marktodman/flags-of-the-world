33d0f5acc59621968a90beee4b2d5ba121afaece
marktodman <drmtodman@gmail.com>
2022-02-20 20:41:23 +0000

    docs: Add flags array objects from A to B

---
+++ assets/js/flags.js
@@ -0,0 +1,74 @@
+let flags = [
+{ 
+    image: 'assets/images/ad.webp',
+    country: 'Andorra',
+},
+{ 
+    image: 'assets/images/ae.webp',
+    country: 'United Arab Emirates',
+},
+{ 
+    image: 'assets/images/af.webp',
+    country: 'Afghanistan',
+},
+{ 
+    image: 'assets/images/ag.webp',
+    country: 'Antigua and Barbuda',
+},
+{ 
+    image: 'assets/images/ai.webp',
+    country: 'Anguilla',
+},
+{ 
+    image: 'assets/images/al.webp',
+    country: 'Albania',
+},
+{ 
+    image: 'assets/images/am.webp',
+    country: 'Armenia',
+},
+{ 
+    image: 'assets/images/ao.webp',
+    country: 'Angola',
+},
+{ 
+    image: 'assets/images/aq.webp',
+    country: 'Antartica',
+},
+{ 
+    image: 'assets/images/ar.webp',
+    country: 'Argentina',
+},
+{ 
+    image: 'assets/images/as.webp',
+    country: 'American Samoa',
+},
+{ 
+    image: 'assets/images/at.webp',
+    country: 'Austria',
+},
+{ 
+    image: 'assets/images/au.webp',
+    country: 'Australia',
+},
+{ 
+    image: 'assets/images/aw.webp',
+    country: 'Aruba',
+},
+{ 
+    image: 'assets/images/ax.webp',
+    country: 'Aland Islands',
+},
+{ 
+    image: 'assets/images/az.webp',
+    country: 'Azerbaijan',
+},
+
+
+
+
+
+
+
+
+]

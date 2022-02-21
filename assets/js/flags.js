5a29916e1451875776ae6d14c6e152fd2b733c84
marktodman <drmtodman@gmail.com>
2022-02-21 20:08:41 +0000

    docs: Complete list of flag objects in flags.js

--- assets/js/flags.js
+++ assets/js/flags.js
@@ -1,3 +1,5 @@
+// Flags downloaded from flagpedia.net. File name retained for simplicity. JavaScript code all original.
+
 let flags = [
 { 
     image: 'assets/images/ad.webp',
@@ -63,12 +65,803 @@ let flags = [
     image: 'assets/images/az.webp',
     country: 'Azerbaijan',
 },
-
-
-
-
-
-
-
-
-]
+{ 
+    image: 'assets/images/ba.webp',
+    country: 'Bosnia and Herzegovina',
+},
+{ 
+    image: 'assets/images/bb.webp',
+    country: 'Barbados',
+},
+{ 
+    image: 'assets/images/bd.webp',
+    country: 'Bangladesh',
+},
+{ 
+    image: 'assets/images/be.webp',
+    country: 'Belgium',
+},
+{ 
+    image: 'assets/images/bf.webp',
+    country: 'Burkina Faso',
+},
+{ 
+    image: 'assets/images/bg.webp',
+    country: 'Bulgaria',
+},
+{ 
+    image: 'assets/images/bh.webp',
+    country: 'Bahrain',
+},
+{ 
+    image: 'assets/images/bi.webp',
+    country: 'Burundi',
+},
+{ 
+    image: 'assets/images/bj.webp',
+    country: 'Benin',
+},
+{ 
+    image: 'assets/images/bm.webp',
+    country: 'Bermuda',
+},
+{ 
+    image: 'assets/images/bn.webp',
+    country: 'Brunei',
+},
+{ 
+    image: 'assets/images/bo.webp',
+    country: 'Bolivia',
+},
+{ 
+    image: 'assets/images/br.webp',
+    country: 'Brazil',
+},
+{ 
+    image: 'assets/images/bs.webp',
+    country: 'Bahamas',
+},
+{ 
+    image: 'assets/images/bt.webp',
+    country: 'Bhutan',
+},
+{ 
+    image: 'assets/images/bw.webp',
+    country: 'Botswana',
+},
+{ 
+    image: 'assets/images/by.webp',
+    country: 'Belarus',
+},
+{ 
+    image: 'assets/images/bz.webp',
+    country: 'Belize',
+},
+{ 
+    image: 'assets/images/ca.webp',
+    country: 'Canada',
+},
+{ 
+    image: 'assets/images/cc.webp',
+    country: 'Cocos Islands',
+},
+{ 
+    image: 'assets/images/cd.webp',
+    country: 'DR Congo',
+},
+{ 
+    image: 'assets/images/cf.webp',
+    country: 'Central African Republic',
+},
+{ 
+    image: 'assets/images/cg.webp',
+    country: 'Republic of the Congo',
+},
+{ 
+    image: 'assets/images/ch.webp',
+    country: 'Switzerland',
+},
+{ 
+    image: 'assets/images/ci.webp',
+    country: "Cote d'Ivoire",
+},
+{ 
+    image: 'assets/images/ck.webp',
+    country: 'Cook Islands',
+},
+{ 
+    image: 'assets/images/cl.webp',
+    country: 'Chile',
+},
+{ 
+    image: 'assets/images/cm.webp',
+    country: 'Cameroon',
+},
+{ 
+    image: 'assets/images/cn.webp',
+    country: 'China',
+},
+{ 
+    image: 'assets/images/co.webp',
+    country: 'Columbia',
+},
+{ 
+    image: 'assets/images/cr.webp',
+    country: 'Costa Rica',
+},
+{ 
+    image: 'assets/images/cu.webp',
+    country: 'Cuba',
+},
+{ 
+    image: 'assets/images/cv.webp',
+    country: 'Cape Verde',
+},
+{ 
+    image: 'assets/images/cw.webp',
+    country: 'Curacao',
+},
+{ 
+    image: 'assets/images/cx.webp',
+    country: 'Christmas Island',
+},
+{ 
+    image: 'assets/images/cy.webp',
+    country: 'Cyprus',
+},
+{ 
+    image: 'assets/images/de.webp',
+    country: 'Germany',
+},
+{ 
+    image: 'assets/images/dj.webp',
+    country: 'Djibouti',
+},
+{ 
+    image: 'assets/images/dk.webp',
+    country: 'Denmark',
+},
+{ 
+    image: 'assets/images/dm.webp',
+    country: 'Dominica',
+},
+{ 
+    image: 'assets/images/do.webp',
+    country: 'Dominican Republic',
+},
+{ 
+    image: 'assets/images/dz.webp',
+    country: 'Algeria',
+},
+{ 
+    image: 'assets/images/ec.webp',
+    country: 'Ecuador',
+},
+{ 
+    image: 'assets/images/ee.webp',
+    country: 'Estonia',
+},
+{ 
+    image: 'assets/images/eg.webp',
+    country: 'Egypt',
+},
+{ 
+    image: 'assets/images/eh.webp',
+    country: 'Western Sahara',
+},
+{ 
+    image: 'assets/images/er.webp',
+    country: 'Eritrea',
+},
+{ 
+    image: 'assets/images/es.webp',
+    country: 'Spain',
+},
+{ 
+    image: 'assets/images/et.webp',
+    country: 'Ethiopia',
+},
+{ 
+    image: 'assets/images/fi.webp',
+    country: 'Finland',
+},
+{ 
+    image: 'assets/images/fj.webp',
+    country: 'Fiji',
+},
+{ 
+    image: 'assets/images/fk.webp',
+    country: 'Falkland Islands',
+},
+{ 
+    image: 'assets/images/fm.webp',
+    country: 'Micronesia',
+},
+{ 
+    image: 'assets/images/fo.webp',
+    country: 'Faroe Islands',
+},
+{ 
+    image: 'assets/images/fr.webp',
+    country: 'France',
+},
+{ 
+    image: 'assets/images/ga.webp',
+    country: 'Gabon',
+},
+{ 
+    image: 'assets/images/gb-eng.webp',
+    country: 'England',
+},
+{ 
+    image: 'assets/images/gb-nir.webp',
+    country: 'Northern Ireland',
+},
+{ 
+    image: 'assets/images/gb-sct.webp',
+    country: 'Scotland',
+},
+{ 
+    image: 'assets/images/gb-wls.webp',
+    country: 'Wales',
+},
+{ 
+    image: 'assets/images/gb.webp',
+    country: 'United Kingdom',
+},
+{ 
+    image: 'assets/images/gd.webp',
+    country: 'Grenada',
+},
+{ 
+    image: 'assets/images/ge.webp',
+    country: 'Georgia',
+},
+{ 
+    image: 'assets/images/gh.webp',
+    country: 'Ghana',
+},
+{ 
+    image: 'assets/images/gi.webp',
+    country: 'Gibraltar',
+},
+{ 
+    image: 'assets/images/gl.webp',
+    country: 'Greenland',
+},
+{ 
+    image: 'assets/images/gm.webp',
+    country: 'Gambia',
+},
+{ 
+    image: 'assets/images/gn.webp',
+    country: 'Guinea',
+},
+{ 
+    image: 'assets/images/gq.webp',
+    country: 'Equatorial Guinea',
+},
+{ 
+    image: 'assets/images/gr.webp',
+    country: 'Greece',
+},
+{ 
+    image: 'assets/images/gt.webp',
+    country: 'Guatemala',
+},
+{ 
+    image: 'assets/images/gw.webp',
+    country: 'Guinea-Bissau',
+},
+{ 
+    image: 'assets/images/gy.webp',
+    country: 'Guyana',
+},
+{ 
+    image: 'assets/images/hk.webp',
+    country: 'Hong Kong',
+},
+{ 
+    image: 'assets/images/hn.webp',
+    country: 'Honduras',
+},
+{ 
+    image: 'assets/images/hr.webp',
+    country: 'Croatia',
+},
+{ 
+    image: 'assets/images/ht.webp',
+    country: 'Haiti',
+},
+{ 
+    image: 'assets/images/hu.webp',
+    country: 'Hungary',
+},
+{ 
+    image: 'assets/images/id.webp',
+    country: 'Indonesia',
+},
+{ 
+    image: 'assets/images/ie.webp',
+    country: 'Ireland',
+},
+{ 
+    image: 'assets/images/il.webp',
+    country: 'Israel',
+},
+{ 
+    image: 'assets/images/im.webp',
+    country: 'Isle of Man',
+},
+{ 
+    image: 'assets/images/in.webp',
+    country: 'India',
+},
+{ 
+    image: 'assets/images/iq.webp',
+    country: 'Iraq',
+},
+{ 
+    image: 'assets/images/ir.webp',
+    country: 'Iran',
+},
+{ 
+    image: 'assets/images/is.webp',
+    country: 'Iceland',
+},
+{ 
+    image: 'assets/images/it.webp',
+    country: 'Italy',
+},
+{ 
+    image: 'assets/images/jm.webp',
+    country: 'Jamaica',
+},
+{ 
+    image: 'assets/images/jp.webp',
+    country: 'Japan',
+},
+{ 
+    image: 'assets/images/ke.webp',
+    country: 'Kenya',
+},
+{ 
+    image: 'assets/images/kg.webp',
+    country: 'Kyrgyzstan',
+},
+{ 
+    image: 'assets/images/kh.webp',
+    country: 'Cambodia',
+},
+{ 
+    image: 'assets/images/ki.webp',
+    country: 'Kiribati',
+},
+{ 
+    image: 'assets/images/kn.webp',
+    country: 'Saint Kitts and Nevis',
+},
+{ 
+    image: 'assets/images/kp.webp',
+    country: 'North Korea',
+},
+{ 
+    image: 'assets/images/kr.webp',
+    country: 'South Korea',
+},
+{ 
+    image: 'assets/images/kw.webp',
+    country: 'Kuwait',
+},
+{ 
+    image: 'assets/images/ky.webp',
+    country: 'Cayman Islands',
+},
+{ 
+    image: 'assets/images/kz.webp',
+    country: 'Kazakhstan',
+},
+{ 
+    image: 'assets/images/la.webp',
+    country: 'Laos',
+},
+{ 
+    image: 'assets/images/lb.webp',
+    country: 'Lebanon',
+},
+{ 
+    image: 'assets/images/lc.webp',
+    country: 'Saint Lucia',
+},
+{ 
+    image: 'assets/images/li.webp',
+    country: 'Liechtenstein',
+},
+{ 
+    image: 'assets/images/ls.webp',
+    country: 'Lesotho',
+},
+{ 
+    image: 'assets/images/lt.webp',
+    country: 'Lithuania',
+},
+{ 
+    image: 'assets/images/lu.webp',
+    country: 'Luxemborg',
+},
+{ 
+    image: 'assets/images/ma.webp',
+    country: 'Morocco',
+},
+{ 
+    image: 'assets/images/md.webp',
+    country: 'Moldova',
+},
+{ 
+    image: 'assets/images/me.webp',
+    country: 'Montenegro',
+},
+{ 
+    image: 'assets/images/mg.webp',
+    country: 'Madagascar',
+},
+{ 
+    image: 'assets/images/mk.webp',
+    country: 'North Macedonia',
+},
+{ 
+    image: 'assets/images/ml.webp',
+    country: 'Mali',
+},
+{ 
+    image: 'assets/images/mm.webp',
+    country: 'Myanmar',
+},
+{ 
+    image: 'assets/images/mo.webp',
+    country: 'Macau',
+},
+{ 
+    image: 'assets/images/mr.webp',
+    country: 'Mauritania',
+},
+{ 
+    image: 'assets/images/ms.webp',
+    country: 'Montserrat',
+},
+{ 
+    image: 'assets/images/mt.webp',
+    country: 'Malta',
+},
+{ 
+    image: 'assets/images/mu.webp',
+    country: 'Mauritius',
+},
+{ 
+    image: 'assets/images/mv.webp',
+    country: 'Maldives',
+},
+{ 
+    image: 'assets/images/mw.webp',
+    country: 'Malawi',
+},
+{ 
+    image: 'assets/images/mx.webp',
+    country: 'Mexico',
+},
+{ 
+    image: 'assets/images/mz.webp',
+    country: 'Mozambique',
+},
+{ 
+    image: 'assets/images/na.webp',
+    country: 'Namibia',
+},
+{ 
+    image: 'assets/images/ne.webp',
+    country: 'Niger',
+},
+{ 
+    image: 'assets/images/nf.webp',
+    country: 'Norfolk Island',
+},
+{ 
+    image: 'assets/images/na.webp',
+    country: 'Nigeria',
+},
+{ 
+    image: 'assets/images/ni.webp',
+    country: 'Nicaragua',
+},
+{ 
+    image: 'assets/images/na.webp',
+    country: 'Namibia',
+},{ 
+    image: 'assets/images/nl.webp',
+    country: 'Netherlands',
+},
+{ 
+    image: 'assets/images/no.webp',
+    country: 'Norway',
+},
+{ 
+    image: 'assets/images/np.webp',
+    country: 'Nepal',
+},
+{ 
+    image: 'assets/images/nr.webp',
+    country: 'Nauru',
+},
+{ 
+    image: 'assets/images/nu.webp',
+    country: 'Niue',
+},
+{ 
+    image: 'assets/images/nz.webp',
+    country: 'New Zealand',
+},
+{ 
+    image: 'assets/images/om.webp',
+    country: 'Oman',
+},
+{ 
+    image: 'assets/images/pa.webp',
+    country: 'Panama',
+},
+{ 
+    image: 'assets/images/pe.webp',
+    country: 'Peru',
+},
+{ 
+    image: 'assets/images/pf.webp',
+    country: 'French Polynesia',
+},
+{ 
+    image: 'assets/images/pg.webp',
+    country: 'Papua New Guinea',
+},
+{ 
+    image: 'assets/images/ph.webp',
+    country: 'Philippines',
+},
+{ 
+    image: 'assets/images/pk.webp',
+    country: 'Pakistan',
+},
+{ 
+    image: 'assets/images/pl.webp',
+    country: 'Poland',
+},
+{ 
+    image: 'assets/images/pn.webp',
+    country: 'Pitcairn Islands',
+},
+{ 
+    image: 'assets/images/pr.webp',
+    country: 'Puerto Rico',
+},
+{ 
+    image: 'assets/images/ps.webp',
+    country: 'Palestine',
+},
+{ 
+    image: 'assets/images/pt.webp',
+    country: 'Portugal',
+},
+{ 
+    image: 'assets/images/pw.webp',
+    country: 'Palau',
+},
+{ 
+    image: 'assets/images/py.webp',
+    country: 'Paraguay',
+},
+{ 
+    image: 'assets/images/qa.webp',
+    country: 'Qatar',
+},
+{ 
+    image: 'assets/images/ro.webp',
+    country: 'Romania',
+},
+{ 
+    image: 'assets/images/rs.webp',
+    country: 'Serbia',
+},
+{ 
+    image: 'assets/images/rw.webp',
+    country: 'Rwanda',
+},
+{ 
+    image: 'assets/images/sa.webp',
+    country: 'Saudi Arabia',
+},
+{ 
+    image: 'assets/images/sb.webp',
+    country: 'Solomon Islands',
+},
+{ 
+    image: 'assets/images/sc.webp',
+    country: 'Seychelles',
+},
+{ 
+    image: 'assets/images/sd.webp',
+    country: 'Sudan',
+},
+{ 
+    image: 'assets/images/se.webp',
+    country: 'Sweden',
+},
+{ 
+    image: 'assets/images/sg.webp',
+    country: 'Singapore',
+},
+{ 
+    image: 'assets/images/si.webp',
+    country: 'Slovenia',
+},
+{ 
+    image: 'assets/images/sk.webp',
+    country: 'Slovakia',
+},
+{ 
+    image: 'assets/images/sl.webp',
+    country: 'Sierra Leone',
+},
+{ 
+    image: 'assets/images/sm.webp',
+    country: 'San Marino',
+},
+{ 
+    image: 'assets/images/so.webp',
+    country: 'Somalia',
+},
+{ 
+    image: 'assets/images/sr.webp',
+    country: 'Suriname',
+},
+{ 
+    image: 'assets/images/ss.webp',
+    country: 'South Sudan',
+},
+{ 
+    image: 'assets/images/sv.webp',
+    country: 'El Salvador',
+},
+{ 
+    image: 'assets/images/sy.webp',
+    country: 'Syria',
+},
+{ 
+    image: 'assets/images/sz.webp',
+    country: 'Eswatini',
+},
+{ 
+    image: 'assets/images/td.webp',
+    country: 'Chad',
+},
+{ 
+    image: 'assets/images/tg.webp',
+    country: 'Togo',
+},
+{ 
+    image: 'assets/images/th.webp',
+    country: 'Thailand',
+},
+{ 
+    image: 'assets/images/tj.webp',
+    country: 'Tajikistan',
+},
+{ 
+    image: 'assets/images/tk.webp',
+    country: 'Tokelau',
+},
+{ 
+    image: 'assets/images/tl.webp',
+    country: 'Timor-Leste',
+},
+{ 
+    image: 'assets/images/tm.webp',
+    country: 'Turkmenistan',
+},
+{ 
+    image: 'assets/images/tn.webp',
+    country: 'Tunisia',
+},
+{ 
+    image: 'assets/images/to.webp',
+    country: 'Tonga',
+},
+{ 
+    image: 'assets/images/tr.webp',
+    country: 'Turkey',
+},
+{ 
+    image: 'assets/images/tt.webp',
+    country: 'Trinidad and Tobago',
+},
+{ 
+    image: 'assets/images/tv.webp',
+    country: 'Tuvalu',
+},
+{ 
+    image: 'assets/images/tw.webp',
+    country: 'Taiwan',
+},
+{ 
+    image: 'assets/images/tz.webp',
+    country: 'Tanzania',
+},
+{ 
+    image: 'assets/images/ua.webp',
+    country: 'Ukraine',
+},
+{ 
+    image: 'assets/images/ug.webp',
+    country: 'Uganda',
+},
+{ 
+    image: 'assets/images/us.webp',
+    country: 'United States of America',
+},
+{ 
+    image: 'assets/images/uy.webp',
+    country: 'Uruguay',
+},
+{ 
+    image: 'assets/images/uz.webp',
+    country: 'Uzbekistan',
+},
+{ 
+    image: 'assets/images/va.webp',
+    country: 'Vatican City',
+},
+{ 
+    image: 'assets/images/vc.webp',
+    country: 'Saint Vincent and the Grenadines',
+},
+{ 
+    image: 'assets/images/ve.webp',
+    country: 'Venezuela',
+},
+{ 
+    image: 'assets/images/vg.webp',
+    country: 'British Virgin Islands',
+},
+{ 
+    image: 'assets/images/vi.webp',
+    country: 'United States Virgin Islands',
+},
+{ 
+    image: 'assets/images/vn.webp',
+    country: 'Vietnam',
+},
+{ 
+    image: 'assets/images/vu.webp',
+    country: 'Vanuatu',
+},
+{ 
+    image: 'assets/images/vs.webp',
+    country: 'Samoa',
+},
+{ 
+    image: 'assets/images/xk.webp',
+    country: 'Kosovo',
+},
+{ 
+    image: 'assets/images/ye.webp',
+    country: 'Yemen',
+},
+{ 
+    image: 'assets/images/za.webp',
+    country: 'South Africa',
+},
+{ 
+    image: 'assets/images/zm.webp',
+    country: 'Zambia',
+},
+{ 
+    image: 'assets/images/zw.webp',
+    country: 'Zimbabwe',
+}
+];

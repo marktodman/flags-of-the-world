f0f2577ee04a06a6fb491a844eb04d24ac711950
marktodman <drmtodman@gmail.com>
2022-03-03 18:24:39 +0000

    feat: Add check answer function and increase score function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,19 @@
-// Function to randomly sort array copied from: https://www.codegrepper.com/code-examples/javascript/how+to+randomly+sort+an+array+javascript 
+// Wait for the DOM to load and get the button elements and add event listeners
+
+// Function to randomly sort array modified from: https://www.codegrepper.com/code-examples/javascript/how+to+randomly+sort+an+array+javascript 
 
 function createNewFlags(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
     while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
     return array;
-  }
+}
 
 let newFlags = createNewFlags(flags);
 console.log(newFlags[0].country);
@@ -28,52 +31,40 @@ function buildFullAnswerArray() {
 let allAnswers = buildFullAnswerArray();
 console.log(allAnswers);
 
-
-// function buildNextQuestionArray() {
-//     let nextQuestionArray = []; {
-//         nextQuestionArray.push(newFlags[0 + 1].country);
-//         nextQuestionArray.push(newFlags[60 + 1].country);
-//         nextQuestionArray.push(newFlags[120 + 1].country);
-//     }
-//     return nextQuestionArray
-// }
-
-// let nextQuestion = buildNextQuestionArray(newFlags);
-// console.log(nextQuestion);
-
 // Shuffle the allAnswers array so that the order of the countries in the answers will be randomised
 function createShuffledAnswers(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
     while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
     return array;
-  }
+}
 
-  let finalAnswers = createShuffledAnswers(allAnswers); 
-  console.log(finalAnswers);
-
-
-  //   Need to iterate through the above adding one each time DO/WHILE TIMER loop?? Although the image comes from +1 on newFlags and the answers come from finalAnswers. checkAnswer will need some thought as the game continues...
-
-  document.getElementById('answer-1').innerText = finalAnswers[0];
-  document.getElementById('answer-2').innerText = finalAnswers[1];
-  document.getElementById('answer-3').innerText = finalAnswers[2];
-  document.getElementById('flag').src = newFlags[0].image;
+let finalAnswers = createShuffledAnswers(allAnswers);
+console.log(finalAnswers);
 
 
+//   Need to iterate through the above adding one each time DO/WHILE TIMER loop?? Although the image comes from +1 on newFlags and the answers come from finalAnswers. checkAnswer will need some thought as the game continues...
+
+document.getElementById('answer-1').innerText = finalAnswers[0];
+document.getElementById('answer-2').innerText = finalAnswers[1];
+document.getElementById('answer-3').innerText = finalAnswers[2];
+document.getElementById('flag').src = newFlags[0].image;
+
+// Code modified from Grepper: https://www.codegrepper.com/code-examples/javascript/add+countdown+timer+to+javascript+quiz
 let count = 60;
-let interval = setInterval(function(){
-  document.getElementById('timer').innerHTML=count;
-  count--;
-  if (count === 0){
-    clearInterval(interval);
-    document.getElementById('timer').innerHTML='GAME OVER'; // this is where I can add what to do once the timer ends - take to the GAME OVER score html page
-  }
+let interval = setInterval(function () {
+    document.getElementById('timer').innerHTML = count;
+    count--;
+    if (count === 0) {
+        clearInterval(interval);
+        document.getElementById('timer').innerHTML = 'GAME OVER'; // this is where I can add what to do once the timer ends - take to the GAME OVER score html page
+    }
 }, 1000);
 
 
@@ -107,21 +98,47 @@ let interval = setInterval(function(){
 // ? If using this method, use a if loop to check if the values are the different...if true use random country, if flase run again?
 
 
+let answer1 = document.getElementById('answer-1');
+let answer2 = document.getElementById('answer-2');
+let answer3 = document.getElementById('answer-3');
 
-
-
-function createFlags() {
-
-}
-
-function runGame() {
-
-}
+answer1.addEventListener('click', checkAnswer);
+answer2.addEventListener('click', checkAnswer);
+answer3.addEventListener('click', checkAnswer);
 
 function checkAnswer() {
-
+    if (this.textContent === newFlags[0].country) {
+        let correct = `CORRECT! <button id="next-question">Next flag >></button>`
+        document.getElementById('result').innerHTML = correct;
+        increaseCorrectScore();
+        buildNextQuestionArray();
+    } else {
+        let incorrect = `WRONG! <button id="next-question">Try the next flag >></button>`
+        document.getElementById('result').innerHTML = incorrect;
+    }
 }
-
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function increaseCorrectScore() {
-
+    let currentScore = parseInt(document.getElementById('correct').innerText);
+    document.getElementById('correct').innerText = ++currentScore;
 }
+
+function buildNextQuestionArray() {
+    let nextQuestionArray = []; 
+    for (question of nextQuestionArray) {
+        nextQuestionArray.push(newFlags[0++].country);
+        nextQuestionArray.push(newFlags[60++].country);
+        nextQuestionArray.push(newFlags[120++].country);
+    }
+    return nextQuestionArray
+}
+
+let nextQuestion = buildNextQuestionArray(newFlags);
+console.log(nextQuestion);
+
+// function nextQuestion() {
+//     if ()
+//  }
+ 

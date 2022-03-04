d33c5a6025bd08d0149923bfda5558c64aac9cb6
marktodman <drmtodman@gmail.com>
2022-03-04 13:58:03 +0000

    fix: Tidy up game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,19 @@
-// Wait for the DOM to load and get the button elements and add event listeners
+/**
+ * Set 60 second countdown timer. Code modified from Grepper: https://www.codegrepper.com/code-examples/javascript/add+countdown+timer+to+javascript+quiz
+ */
+let count = 60;
+let interval = setInterval(function () {
+    document.getElementById('timer').innerHTML = count;
+    count--;
+    if (count === 0) {
+        clearInterval(interval);
+        document.getElementById('timer').innerHTML = 'GAME OVER'; // this is where I can add what to do once the timer ends - take to the GAME OVER score html page
+    }
+}, 1000);
 
-// Function to randomly sort array modified from: https://www.codegrepper.com/code-examples/javascript/how+to+randomly+sort+an+array+javascript 
-
+/**
+ * Function to randomly sort array modified from: https://www.codegrepper.com/code-examples/javascript/how+to+randomly+sort+an+array+javascript 
+*/
 function createNewFlags(array) {
     var currentIndex = array.length,
         temporaryValue, randomIndex;
@@ -18,7 +30,10 @@ function createNewFlags(array) {
 let newFlags = createNewFlags(flags);
 console.log(newFlags[0].country);
 
-// Create an array of the correct answer and two wrong answers
+
+/** 
+ * Create an array of the correct answer and two wrong answers
+ **/
 function buildFullAnswerArray() {
     let fullAnswerArray = []; {
         fullAnswerArray.push(newFlags[0].country);
@@ -56,35 +71,12 @@ document.getElementById('answer-2').innerText = finalAnswers[1];
 document.getElementById('answer-3').innerText = finalAnswers[2];
 document.getElementById('flag').src = newFlags[0].image;
 
-// Code modified from Grepper: https://www.codegrepper.com/code-examples/javascript/add+countdown+timer+to+javascript+quiz
-let count = 60;
-let interval = setInterval(function () {
-    document.getElementById('timer').innerHTML = count;
-    count--;
-    if (count === 0) {
-        clearInterval(interval);
-        document.getElementById('timer').innerHTML = 'GAME OVER'; // this is where I can add what to do once the timer ends - take to the GAME OVER score html page
-    }
-}, 1000);
-
-
-
-//   Need to iterate through the above adding one each time DO/WHILE TIMER loop??
-
-
-// Create an array of the three countries from random contries and then populate the three HTML questions?
-
-// How to change which one is correct? 1/2/3?? - easy way would be to write three incorrect answers in the object...
-
 // Take one correct country from the shuffled array using index[0] and then plus one for each new set of questions. Display flag from index[0] in div
 // Take two incorrect countries from the shuffled array using index [60] and index [120] and then plus one for each new set of questions
 // Add all of them to an array - the correct country plus two incorrect countries
 // Create a new random array from the newly created array of three
 
-// Check button onclick whether correct answer or not - event listner
-// If correct - return CORRECT! for 500 milliseconds and advance to next question
-// If incorrect - return INCORRECT! Correct answer was `${correctAnswer}` for 500 milliseconds and advance to next question
-// Continue until timer 6000 milliseconds runs out
+
 
 // let randomCountryOne = Math.floor(Math.random() * flags.length);
 // let randomCountryTwo = Math.floor(Math.random() * flags.length);
@@ -106,12 +98,16 @@ answer1.addEventListener('click', checkAnswer);
 answer2.addEventListener('click', checkAnswer);
 answer3.addEventListener('click', checkAnswer);
 
+/** 
+ * Check button onclick whether correct answer or not - event listner
+ * If correct - return CORRECT! for 500 milliseconds and advance to next question
+ * If incorrect - return WRONG!
+**/ 
 function checkAnswer() {
     if (this.textContent === newFlags[0].country) {
         let correct = `CORRECT! <button id="next-question">Next flag >></button>`
         document.getElementById('result').innerHTML = correct;
         increaseCorrectScore();
-        buildNextQuestionArray();
     } else {
         let incorrect = `WRONG! <button id="next-question">Try the next flag >></button>`
         document.getElementById('result').innerHTML = incorrect;
@@ -125,18 +121,27 @@ function increaseCorrectScore() {
     document.getElementById('correct').innerText = ++currentScore;
 }
 
-function buildNextQuestionArray() {
-    let nextQuestionArray = []; 
-    for (question of nextQuestionArray) {
-        nextQuestionArray.push(newFlags[0++].country);
-        nextQuestionArray.push(newFlags[60++].country);
-        nextQuestionArray.push(newFlags[120++].country);
-    }
-    return nextQuestionArray
-}
+// let nextFlag = document.getElementById('next-question');
+// nextFlag.addEventListener('click', nextQuestion);
 
-let nextQuestion = buildNextQuestionArray(newFlags);
-console.log(nextQuestion);
+// function nextQuestion(){
+//     createNewFlags();
+//     buildFullAnswerArray();
+//     createShuffledAnswers();
+// }
+
+// function buildNextQuestionArray() {
+//     let nextQuestionArray = []; 
+//     for (question of nextQuestionArray) {
+//         nextQuestionArray.push(newFlags[0++].country);
+//         nextQuestionArray.push(newFlags[60++].country);
+//         nextQuestionArray.push(newFlags[120++].country);
+//     }
+//     return nextQuestionArray
+// }
+
+// let nextQuestion = buildNextQuestionArray(newFlags);
+// console.log(nextQuestion);
 
 // function nextQuestion() {
 //     if ()
